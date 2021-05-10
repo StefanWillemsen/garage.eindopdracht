4b171e6b712104603c40f5ffcb4df522eac8a20c
stefan willemsen <stefan.willemsen@hotmail.com>
2021-05-10 11:11:34 +0200

    added application properties and other needed files

---
+++ src/main/java/nl/novi/springboot/eindopdracht/GarageApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.springboot.eindopdracht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarageApplication.class, args);
+	}
+
+}
