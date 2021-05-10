efceb7d2be95487b94fb99dc4ddd939a47c58641
stefan willemsen <stefan.willemsen@hotmail.com>
2021-05-10 11:18:07 +0200

    added EmployeeTest

---
+++ src/test/java/model/EmployeeTest.java
@@ -0,0 +1,56 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeTest {
+
+    @Test
+    void getEmployeeID() {
+    }
+
+    @Test
+    void setEmployeeID() {
+    }
+
+    @Test
+    void getFirstName() {
+    }
+
+    @Test
+    void setFirstName() {
+    }
+
+    @Test
+    void getLastName() {
+    }
+
+    @Test
+    void setLastName() {
+    }
+
+    @Test
+    void getPhoneNumber() {
+    }
+
+    @Test
+    void setPhoneNumber() {
+    }
+
+    @Test
+    void getRole() {
+    }
+
+    @Test
+    void setRole() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+}
