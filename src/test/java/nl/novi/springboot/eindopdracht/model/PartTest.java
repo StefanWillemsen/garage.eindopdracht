dc395fa0c4e6c261c0cc155b05b0a84036ecee28
stefan willemsen <stefan.willemsen@hotmail.com>
2021-07-29 16:09:50 +0200

    partTest added some tests

--- src/test/java/nl/novi/springboot/eindopdracht/model/PartTest.java
+++ src/test/java/nl/novi/springboot/eindopdracht/model/PartTest.java
@@ -1,48 +1,34 @@
 package nl.novi.springboot.eindopdracht.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PartTest {
-
+ private Part part;
+ @BeforeEach
+ void setUp(){
+     this.part = new Part();
+     this.part.setPartId(5);
+     this.part.setName("v-belt");
+     this.part.setPrice(15.15);
+ }
     @Test
     void getPartID() {
+     assertEquals(5,this.part.getPartID());
     }
 
-    @Test
-    void setPartId() {
-    }
 
     @Test
     void getName() {
-    }
-
-    @Test
-    void setName() {
-    }
-
-    @Test
-    void getDescription() {
-    }
-
-    @Test
-    void setDescription() {
+     assertEquals("v-belt", this.part.getName());
     }
 
     @Test
     void getPrice() {
+     assertEquals(15.15, this.part.getPrice());
     }
 
-    @Test
-    void setPrice() {
-    }
 
-    @Test
-    void getMaintenances() {
-    }
-
-    @Test
-    void setMaintenances() {
-    }
 }
